b0f3d04a1d5bdd33175e993bdbfc11e64b2d7475
ivonamilutinovic <ivona.milutinovic95@gmail.com>
2019-12-02 22:44:14 +0100

    Added base MVC model for ClientApplication

---
+++ VisokiNapon/ClientApplication/Controllers/ClientController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientApplication.Controllers
+{
+    public class ClientController{
+
+        private readonly ILogger<PlayerController> _logger;
+
+        public PlayerController(ILogger<PlayerController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Player()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
---
+++ VisokiNapon/ClientApplication/Controllers/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using ClientApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace ClientApplication.Controllers
+{
+    class PlayerController : Controller
+    {
+
+        private readonly ILogger<PlayerController> _logger;
+
+        public PlayerController(ILogger<PlayerController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Answering()
+        {
+            return View();
+        }
+
+        public IActionResult MatrixOfQuestions()
+        {
+            return View();
+        }
+
+        public IActionResult ModeChoosing()
+        {
+            return View();
+        }
+
+        public IActionResult Question()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
