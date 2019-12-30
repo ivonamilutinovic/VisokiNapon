66b7a0c9d60f4d25bba8dfe7cdf2bea6af5bd5c9
LMladenovic <lazar.s.mladenovic@hotmail.com>
2019-12-30 18:51:29 +0100

    added funcionality - send question to desired client

---
+++ VisokiNapon/VisokiNapon/Controllers/QuestionController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using VisokiNapon.Data;
+using VisokiNapon.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QuestionController : Controller
+    {
+        private readonly ILogger<QuestionController> _logger;
+
+        public QuestionController(ILogger<QuestionController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet()]
+        public String Get()
+        {
+            return "Upotreba host/quesition/clientId/valueOfQuestion\n" +
+                   "Ovaj metod ce kasnije biti obrisan, sad je tu fore radi!";
+        }
+
+        [HttpGet("{id}/{idQ}")]
+        public Question Get(int id, int idQ)
+        {
+            // QuestionsForClientRepo hlp = new QuestionsForClientRepo();
+            // return hlp.getQuestionForClientIdAndValue(id, idQ, num);
+
+            return QuestionsForClientRepo.getQuestionForClientIdAndValue(id, idQ);
+
+        }
+
+    }
+}
---
+++ VisokiNapon/VisokiNapon/Data/QuestionsForClientRepo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VisokiNapon.Models;
+
+namespace VisokiNapon.Data
+{
+    public class QuestionsForClientRepo
+    {
+
+        public static QuestionsForClient[] questionsForCurrentClients =
+            new QuestionsForClient[] { 
+                /* prvi klijent, ide neki njegov id, a u QuestionsForClient se generisu pitanja */
+                new QuestionsForClient(1),
+                /* drugi klijent */
+                new QuestionsForClient(2),
+                /* treci klijent */
+                new QuestionsForClient(3),
+            };
+
+
+        public static Question getQuestionForClientIdAndValue(int clientId, int value)
+        {
+            Question question = new Question();
+
+            /* indeks u nizu gde su podaci klijenta sa clientId */
+            int index = Array.FindIndex(questionsForCurrentClients, element => element.clientId == clientId);
+
+
+            /* OVDE BI INACE ISO DISCONNECT */
+            if (index == -1)
+            {
+                question.question = "THERE IS NO CLIENT WITH PROCEEDED ID ";
+                return question;
+            }
+
+            if (value == 10000 && questionsForCurrentClients[index].q10 != 5)
+            {
+                question = questionsForCurrentClients[index].questions[questionsForCurrentClients[index].q10];
+                questionsForCurrentClients[index].q10++;
+
+            }
+
+            if (value == 50000 && questionsForCurrentClients[index].q50 != 5)
+            {
+                question = questionsForCurrentClients[index].questions[questionsForCurrentClients[index].q50 + 5];
+                questionsForCurrentClients[index].q50++;
+
+            }
+
+            if (value == 100000 && questionsForCurrentClients[index].q100 != 5)
+            {
+                question = questionsForCurrentClients[index].questions[questionsForCurrentClients[index].q100 + 10];
+                questionsForCurrentClients[index].q100++;
+
+            }
+
+            /* OVDE BI INACE ISO DISCONNECT */
+            if (question.question == null)
+                question.question = "YOU HAVE ANSWERED TO ALL QUESTIONS FOR PROCEEDED AMOUNT!";
+
+            return question;
+
+        }
+
+
+    }
+}
