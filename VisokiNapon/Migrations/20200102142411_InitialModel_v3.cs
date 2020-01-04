1ab4a94b16ccf9c1ca95198a06279c954ac64ae4
goran-milenkovic <goran.milenkovic95@outlook.com>
2020-01-04 21:52:14 +0100

    integrated Entity Framework Core 3.0 and added initial base model for questions

---
+++ VisokiNapon/Migrations/20200102142411_InitialModel_v3.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VISOKI_NAPON.Migrations
+{
+    public partial class InitialModel_v3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Questions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                        //.Annotation("SqlServer:Identity", "1, 1"),
+                    Text = table.Column<string>(nullable: true),
+                    Answer = table.Column<string>(nullable: true),
+                    Category = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Questions", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Questions");
+        }
+    }
+}
